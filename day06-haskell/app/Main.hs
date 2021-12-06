d5640ad17e4a238f21cf462495964011c0ec704f
William Guimont-Martin <william.guimont-martin.1@ulaval.ca>
2021-12-06 09:05:53 -0500

    planned part 2

--- day06-haskell/app/Main.hs
+++ day06-haskell/app/Main.hs
@@ -14,6 +14,8 @@ stepAll xs = map step xs <> replicate numNew 8
 naive :: Int -> [Int] -> Int
 naive d lanterns = length $ foldl (\c _ -> stepAll c) lanterns [1..d]
 
+-- TODO array of contribution at each day (mod 7) + array of future 
+
 main :: IO ()
 main = do
   input <- readFile "test.txt"
