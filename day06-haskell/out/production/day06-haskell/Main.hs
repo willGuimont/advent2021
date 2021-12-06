9dcd043e3b736ea994175bd565cecb18d78dd64a
William Guimont-Martin <william.guimont-martin.1@ulaval.ca>
2021-12-06 15:15:03 -0500

    day 06 part 2

--- day06-haskell/out/production/day06-haskell/Main.hs
+++ day06-haskell/out/production/day06-haskell/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 
+import Data.List
 import Data.List.Split
+import qualified Data.MultiSet as M
 
+-- Naive solution
 step :: Int -> Int
 step 0 = 6
 step x = x - 1
@@ -12,10 +15,17 @@ stepAll xs = map step xs <> replicate numNew 8
     numNew = length $ filter (== 0) xs
 
 naive :: Int -> [Int] -> Int
-naive d lanterns = length $ foldl (\c _ -> stepAll c) lanterns [1..d]
+naive d lanterns = length $ foldl (\c _ -> stepAll c) lanterns [1 .. d]
 
+-- A better solution
+smarter :: M.MultiSet Integer -> M.MultiSet Integer
+smarter = M.concatMap (\i -> if i == 0 then [6, 8] else [i - 1])
+
+-- Main
 main :: IO ()
 main = do
-  input <- readFile "test.txt"
-  let lanterns = map read $ splitOn "," $input :: [Int]
-   in print $ naive 80 lanterns
+  input <- readFile "input.txt"
+  let lanterns = map read $ splitOn "," $input :: [Integer]
+   in do
+        print $ naive 80 (map fromInteger lanterns)
+        print $ M.size $ (!! 256) $ iterate smarter $ M.fromList lanterns
