cbf4c68faa15d40be4e123752873b7a1ce7b5f2e
William Guimont-Martin <william.guimont-martin.1@ulaval.ca>
2021-12-06 08:31:20 -0500

    day06 part 1

---
+++ day06-haskell/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for day06-haskell
+
+## Unreleased changes
---
+++ day06-haskell/app/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Data.List.Split
+
+step :: Int -> Int
+step 0 = 6
+step x = x - 1
+
+stepAll :: [Int] -> [Int]
+stepAll xs = map step xs <> replicate numNew 8
+  where
+    numNew = length $ filter (== 0) xs
+
+naive :: Int -> [Int] -> Int
+naive d lanterns = length $ foldl (\c _ -> stepAll c) lanterns [1..d]
+
+main :: IO ()
+main = do
+  input <- readFile "test.txt"
+  let lanterns = map read $ splitOn "," $input :: [Int]
+   in print $ naive 80 lanterns
